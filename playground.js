0973309601e317d23c2423dc164cdd13bf63770e
Aaron Peck <aaronpeck_14@yahoo.com>
2025-03-10 02:30:13 -0700

    chore: update pg

--- playground.js
+++ playground.js
@@ -19,69 +19,77 @@ await connectDB()
 
 async function playground() {
   // ! ---------------------- READ FROM JSON -------------------------------
-  try {
-    const PLAYBYPLAYs = await PlayByPlay.find({}).lean()
-    // TODO: possessions are not changing properly
-    // TODO: homePts and awayPts should never be null, unless coin toss
-    const files = await readdir(path.join(process.cwd(), "data/newPlays"))
-    const updates = []
-    for (const f of files) {
-      console.log(f)
-      const newPlayByPlay = loadJSON(`../../data/newPlays/${f}`)
-      const oldPlayByPlay = PLAYBYPLAYs.find((g) => g.pfr === newPlayByPlay.pfr)
-      if (!oldPlayByPlay) continue
-      const updatedPlays = []
-      const teams = getTeamAbbreviationsFromPlays(oldPlayByPlay.plays)
-      const receivingTeam = getReceivingTeamFromDetail(oldPlayByPlay.plays[0].detail)
-      const receivingTeamAbbr = getTeamAbbrByName(receivingTeam)
-      let currentPosIndex = teams.indexOf(receivingTeamAbbr)
-      const score = {
-        away: 0,
-        home: 0,
-      }
-      const filteredNewPlays = newPlayByPlay.plays.filter((p) => p)
-
-      if (filteredNewPlays.length !== oldPlayByPlay.plays.length) {
-        console.log(newPlayByPlay.pfr, filteredNewPlays.length, oldPlayByPlay.plays.length)
-        continue
-      }
-
-      for (let i = 0; i < filteredNewPlays.length; i++) {
-        const newPlay = filteredNewPlays[i]
-        const oldPlay = oldPlayByPlay.plays[i]
-        const updatedPlay = { ...oldPlay }
-        const isCoinToss = newPlay?.detail.includes("coin toss")
-
-        // fill in missing scores (non-coin toss entries) & update score
-        if (!isCoinToss) {
-          if (updatedPlay.homePts === null) {
-            if (newPlay.home) {
-              score.home = parseInt(newPlay.home)
-            }
-            updatedPlay.homePts = score.home
-          }
-          if (updatedPlay.awayPts === null) {
-            if (newPlay.away) {
-              score.away = parseInt(newPlay.away)
-            }
-            updatedPlay.awayPts = score.away
-          }
-        }
-        if (newPlay?.isOvertime) updatedPlay.quarter = 5
-        if (newPlay?.changeOfPos) {
-          currentPosIndex = currentPosIndex === 0 ? 1 : 0
-        }
-        updatedPlay.possession = isCoinToss ? null : teams[currentPosIndex]
-        updatedPlays.push(updatedPlay)
-      }
-      updates.push({ pfr: oldPlayByPlay.pfr, updatedPlays })
-    }
-
-    await writeFile(path.join(process.cwd(), "data/revisedPlays.json"), JSON.stringify(updates, null, 2), "utf-8")
-    console.log("Updated plays written to /data/revisedPlays")
-  } catch (error) {
-    console.error("Error processing plays:", error)
-  }
+  // try {
+  //   const PLAYBYPLAYs = await PlayByPlay.find({}).lean()
+  //   const files = await readdir(path.join(process.cwd(), "data/newPlays"))
+  //   const updates = []
+  //   for (const f of files) {
+  //     const newPlayByPlay = loadJSON(`../../data/newPlays/${f}`)
+  //     const oldPlayByPlay = PLAYBYPLAYs.find((g) => g.pfr === newPlayByPlay.pfr)
+  //     if (!oldPlayByPlay) continue
+  //     const updatedPlays = []
+  //     const teams = getTeamAbbreviationsFromPlays(oldPlayByPlay.plays)
+  //     const receivingTeam = getReceivingTeamFromDetail(oldPlayByPlay.plays[0].detail)
+  //     const receivingTeamAbbr = getTeamAbbrByName(receivingTeam, teams)
+  //     const receivingTeamIndex = teams.indexOf(receivingTeamAbbr)
+  //     let currentPosIndex = receivingTeamIndex
+  //     const score = {
+  //       away: 0,
+  //       home: 0,
+  //     }
+  //     const filteredNewPlays = newPlayByPlay.plays.filter((p) => p)
+  //     if (filteredNewPlays.length !== oldPlayByPlay.plays.length) {
+  //       console.log(newPlayByPlay.pfr, filteredNewPlays.length, oldPlayByPlay.plays.length)
+  //       continue
+  //     }
+  //     for (let i = 0; i < filteredNewPlays.length; i++) {
+  //       try {
+  //         const newPlay = filteredNewPlays[i]
+  //         const oldPlay = oldPlayByPlay.plays[i]
+  //         const updatedPlay = { ...oldPlay }
+  //         const isCoinToss = newPlay?.detail.includes("coin toss")
+  //         if (!isCoinToss) {
+  //           // fill in missing scores (non-coin toss entries) & update score
+  //           if (updatedPlay.homePts === null) {
+  //             if (newPlay.home) {
+  //               score.home = parseInt(newPlay.home)
+  //             }
+  //             updatedPlay.homePts = score.home
+  //           }
+  //           if (updatedPlay.awayPts === null) {
+  //             if (newPlay.away) {
+  //               score.away = parseInt(newPlay.away)
+  //             }
+  //             updatedPlay.awayPts = score.away
+  //           }
+  //         }
+  //         if (newPlay?.isOvertime) {
+  //           if (isCoinToss) {
+  //             const otRecTeam = getReceivingTeamFromDetail(newPlay.detail)
+  //             const otRecTeamAbbr = getTeamAbbrByName(otRecTeam, teams)
+  //             const otRecTeamIndex = teams.indexOf(otRecTeamAbbr)
+  //             currentPosIndex = otRecTeamIndex
+  //           }
+  //           updatedPlay.quarter = 5
+  //         }
+  //         if (newPlay?.changeOfPos) {
+  //           if (newPlay?.startOfThirdQuarter) {
+  //             currentPosIndex = receivingTeamIndex === 0 ? 1 : 0
+  //           } else currentPosIndex = currentPosIndex === 0 ? 1 : 0
+  //         }
+  //         updatedPlay.possession = isCoinToss ? null : teams[currentPosIndex]
+  //         updatedPlays.push(updatedPlay)
+  //       } catch (e) {
+  //         console.log(e)
+  //       }
+  //     }
+  //     updates.push({ pfr: oldPlayByPlay.pfr, updatedPlays })
+  //   }
+  //   await writeFile(path.join(process.cwd(), "data/revisedPlays.json"), JSON.stringify(updates, null, 2), "utf-8")
+  //   console.log("Updated plays written to /data/revisedPlays")
+  // } catch (error) {
+  //   console.error("Error processing plays:", error)
+  // }
   // ! ---------------------- READ FROM CSV -------------------------------
   // fs.readdir("data/brokenCSVs", async (err, files) => {
   //   if (err) {
@@ -119,6 +127,12 @@ async function playground() {
   //       })
   //   }
   // })
+  // const revisedPlays = loadJSON("../../data/revisedPlays.json")
+  // const updates = revisedPlays.map(({ pfr, updatedPlays }) =>
+  //   PlayByPlay.updateOne({ pfr }, { $set: { plays: updatedPlays } }),
+  // )
+  // await Promise.all(updates)
+  // console.log("Done!")
 }
 
 playground()
